d0b73f49e9ca4ad97854fdce2bb555c8c82fba44
LYL <1056660519@qq.com>
2016-09-09 16:05:02 +0800

    editable Toast beta v1.0

---
+++ statics/scripts/demo.js
@@ -0,0 +1,62 @@
+/**
+ * Created by LONG on 2016/8/4.
+ */
+
+$(document).ready(function(){
+    var eToast = EToast(".toast-container",{
+        successImg:"statics/images/success-tip.png",
+        errorImg:"statics/images/error-tip.png",
+        loadingImg:"statics/images/loading-tip.gif",
+        successTip:"成功"
+    });
+    $(".main .title").html(common.textTitle);
+    $(".main .typeIn-area textarea").attr("placeholder",common.textTip);
+    $('.typeIn-area textarea').on("keyup change",function(){
+        var currentLenght = $(this).val().length;
+        $(".current-count").html(currentLenght);
+        if(currentLenght>=common.textLimit){
+            $(".current-count").addClass("reach-limit");
+            $(".publish").addClass("reach-limit");
+        }else{
+            $(".current-count").removeClass("reach-limit");
+            $(".publish").removeClass("reach-limit");
+        }
+    });
+
+    $(".publish").on("touchend click",function(){
+        if($(this).attr("class").indexOf("reach-limit")==-1){
+            eToast.error("字数不得少于"+common.textLimit+"字");
+            return;
+        }
+        publish();
+    });
+
+function publish(){
+    eToast.loading("读取中");
+    $.ajax({
+        type:"POST",
+        url:common.publishUrl,
+        //可选参数data由于测试用，所以未填写
+        //data:{"content":$(".typeIn-area textarea").val()},
+        dataType:"json",
+        success:function(data){
+            eToast.loaded();
+            if(data&&data.rescode==1){
+                eToast.success(data.resmsg);
+                // eToast.success();
+            }else if(data){
+                eToast.error(data.resmsg);
+            }else{
+                eToast.error("操作失败");
+            }
+        },
+        error:function(data){
+            eToast.loaded();
+            if(data){
+                eToast.error(data.resmsg);
+            }
+        }
+    });
+}
+
+});
